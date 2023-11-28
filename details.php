618c2146504b1e36d5b7baacb7bdf54894bea97a
Victor Kituku <victorkituku@gmail.com>
2023-11-28 17:58:11 +0300

    deleting a single record from the database using a hidden input field to capture the id of the record to delte

--- details.php
+++ details.php
@@ -1,6 +1,20 @@
 <?php
 include('config/db_connect.php');
 
+if(isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+    //make sql
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)){
+        //success
+        header('Location: Index.php');
+    }else{
+        //failure
+        echo 'query error: ' . mysqli_error($conn);
+    }
+}
+
 //check GET request id param
 if(isset($_GET['id'])){
     $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -33,6 +47,12 @@ if(isset($_GET['id'])){
         <p><?php echo date($pizza['created_at']) ?></p>
         <h5>Ingredients</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+        <!-- DELETE FORM -->
+        <form action="details.php" method="POST">
+            <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+            <input type="submit" name="delete" class="btn brand z-depth-0" value="Delete">
+        </form>
     <?php else: ?>
         <h5>No such pizza exists!</h5>
     <?php endif; ?>
